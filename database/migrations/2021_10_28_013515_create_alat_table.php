44d7a0bc37bb04fbb5b7fd437d4e0a119afaac2d
unknown <huda.saputra@pln.co.id>
2021-12-16 07:57:46 +0700

    first commit

---
+++ database/migrations/2021_10_28_013515_create_alat_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlatTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alat', function (Blueprint $table) {
+            $table->integer('id_alat')->primary();
+            $table->string('nama_alat');
+            $table->integer('jumlah_alat')->unsigned()->nullable()->default(0);
+            $table->text('deskripsi')->nullable();
+            $table->string('lokasi')->nullable();
+            $table->text('cover')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alat');
+    }
+}
